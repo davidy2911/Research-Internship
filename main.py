2049193bd112a3e764200442522089b100eea4e9
davidy2911 <58455322+davidy2911@users.noreply.github.com>
2020-04-13 12:27:36 +0800

    Added Fast Fourier Transform Solver
    Added two solvers that utilise the fact that M is a circulant matrix. FFT uses the Fast Fourier Transsform to first caclulate the eigen values of M, and the then uses it again to solve as each x is generated. FFTInv uses the Fast Fourier Transform method to cacualte the inverse of M, and then uses the inverse to solve for each x generated. Also added comments for main.

--- main.py
+++ main.py
@@ -4,30 +4,48 @@ Created on Mon Apr  6 18:19:11 2020
 
 @author: David
 """
-import create_matrix as cm
-import time as t
-import numpy as np
-import distributions as dist
-import solvers 
-import approximate as a
+
+"""First I import some standard scientific modules for  timing, for dealing with arrays
+ and for reading input off command line """
+
+import time as t          
+import numpy as np          
 import sys
+
+"""Then I import the modules I have written for this project that handle creating the 
+large sparse matrix, generating samples from the distributions, solving the linear system, 
+and combining these to do the actaul monte carlo simualtion and approximation"""
+
+import create_matrix as cm
+import distributions as dist
+import solvers as sol
+import approximate as app
+
+""""I create a dictionary of possible inputs from the command line and the functions and 
+classes they respsond to in the distributions and solvers modules"""
  
 d = { 'normal': dist.Normal,  'expradial': dist.ExpRadial, 'weibullradial' : dist.WeibullRadial}
-s = { 'lu': solvers.LU, 'factor': solvers.Factorize}
+s = { 'lu': sol.LU, 'factor': sol.Factorize, 'fft': sol.FFT, 'fft_inv': sol.FFTInv}
 
+"""These iterative generators are then assigned to variables"""
 
 distribution = d[sys.argv[1]]
 solver = s[sys.argv[2]]
 matrix_size =  int(sys.argv[3])
+
+"""The step size to move the alpha and the number of iterations are hard coded in as 
+I rarley need to change them """
 
 step_size = 0.1
 num_steps = int(1/(2*step_size))
 
 num_iterations = 1000000
 
+"""I declare the empty lists that values will be appended too"""
+
 actual_det=[]
 alpha=[]
-Y=[]
+approximated_det=[]
 
 for i in range(1, num_steps):
     
@@ -38,16 +56,15 @@ for i in range(1, num_steps):
     M = cm.create_matrix(matrix_size, alpha[i-1])
     
     actual_det.append(np.linalg.det(M.todense()))
-    
 
     begin = t.time()
     
-    Y.append(a.approximate(distribution, solver, M, num_iterations, 1))
+    approximated_det.append(app.approximate(distribution, solver, M, num_iterations, 1))
     
     my = t.time() - begin
 
     print('Analytically Computed Determinant: ' ,actual_det[i-1])
-    print('Approximated Determinant: ', Y[i-1])
+    print('Approximated Determinant: ', approximated_det[i-1])
     print('Time to Approximate: ', my)
 
-print(actual_det, Y, sep = '\n')
+print(actual_det, approximated_det, sep = '\n')
--- solvers.py
+++ solvers.py
@@ -5,8 +5,10 @@ Created on Wed Apr  8 15:10:51 2020
 @author: David
 """
 
+import scipy.sparse as sparse
 import scipy.sparse.linalg as spalg
-
+import scipy.fft as scifft
+import numpy as np
 class LU:
     def __init__(self, M):
         self.LU = spalg.splu(M)
@@ -23,3 +25,34 @@ class Factorize:
     def solve(self, x):
         y = self.factorized(x)
         return y
+
+class FFT:
+    def __init__(self, M):
+        self.diag_inv = np.reciprocal(np.real(scifft.fft(M[:,0].todense().T)[0]))
+
+    def solve(self, x):
+        v = scifft.fft(x)
+        u = self.diag_inv*v
+        y = scifft.ifft(u)
+        return np.real(y)
+         
+        
+class FFTInv:
+    def __init__(self, M):
+        diag_inv = sparse.diags(np.reciprocal(np.real(scifft.fft(M[:,0].todense().T)[0]))).todense()
+        A = scifft.ifft(diag_inv).T
+        self.inv = np.real(scifft.fft(A))
+
+    def solve(self, x):
+        y = np.dot(self.inv, x)
+        return y
+
+if __name__ == '__main__':
+    import create_matrix as cm
+    n = 4 
+    M = cm.create_matrix(n,0.1)
+    solver =  FFTInv(M)
+    print(solver.inv)
+    print(solver.solve(x))
+
+
